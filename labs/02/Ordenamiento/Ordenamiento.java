4ff2b3e335f5a49a7351cb4300fd1a2927b514d4
JACOB1705 <141367280+JACOB1705@users.noreply.github.com>
2023-09-06 19:56:20 -0500

    Segundo Commit ordenamiento

--- labs/02/Ordenamiento/Ordenamiento.java
+++ labs/02/Ordenamiento/Ordenamiento.java
@@ -2,6 +2,21 @@ package com.mycompany.ordenamiento;
 import java.util.Random;
 
 public class Ordenamiento {
-
-   
+    public static void main(String[] args) {
+         int[] size = {100, 500, 1000, 5000, 10000};
+         
+    }
+  
+    public static void ordenamientoBurbuja(double[] arreglo) {
+        for (int i = 0; i < arreglo.length-1; i++) {
+            for (int j = 0; j < arreglo.length-1-i; j++) {
+                if (arreglo[j] > arreglo[j+1]) {
+                    double temp = arreglo[j];
+                    arreglo[j] = arreglo[j+1];
+                    arreglo[j+1] = temp;
+                    }
+                }
+            }
+        }
+    
 }
