cebc82597540f46af3350757f8f1ba828a14a76d
JACOB1705 <141367280+JACOB1705@users.noreply.github.com>
2023-09-07 15:25:47 -0500

    Add files via upload

--- labs/02/01/Captura.java
+++ labs/02/01/Captura.java
@@ -11,9 +11,28 @@ class Captura {
   public static double [][] readMat(int filas, int cols) {
     double [][] matR = new double[filas][cols];
 
-    
+    for(int i=0;i<filas;i++){
+        for(int j=0;j<cols; j++){
+            System.out.println("Ingrese el numero en la fila" + (i+1) + "Ingrese el numero en la columna" + (j+1) + ": ");
+            matR[i][j]=leer.nextDouble();
+        }
+    }
     return matR;
   }
+   public static double Capescalar(){
+      System.out.println("Ingrese un escalar: ");
+      leer.nextDouble();
+   
+      return leer.nextDouble();
+  }
+      public static double Capopcion(){
+      System.out.println("Ingrese un escalar: ");
+      leer.nextDouble();
+      return leer.nextDouble();
+  }
+  
+  
+ 
 }
 
 
--- labs/02/01/Implementacion.java
+++ labs/02/01/Implementacion.java
@@ -1,19 +1,70 @@
 package com.mycompany.opmatriz;
+import com.mycompany.opmatriz.Captura;
+import com.mycompany.opmatriz.OpMatriz;
 
 class Implementacion {
      public static void main(String args[]) {
-
-        /*
-       Montar aquí un menú repetitivo que maneje cada una de las opciones referidas.
-
-       Dependiendo de la opción escogida, leer: dos matrices, una matriz y un escalar o,
-       una sola matriz.
-
-       Luego, proceder a realizar la operación correspondiente y presentar su resultado.
-
-       Se recomienda usar las clases referidas anteriormente par facilitar el desarrollo
-       del programa (algunos métodos se requiere completarlos)
-    */
-
+         System.out.println("Menú de Operaciones de Matrices");
+         System.out.println("1. Sumar dos matrices");
+         System.out.println("2. Multiplicar dos matrices");
+         System.out.println("3. Multiplicar una matriz por un escalar");
+         System.out.println("4. Calcular la transposición de una matriz");
+         System.out.println("5. Salir");
+         System.out.print("Seleccione una opción: ");
+         double opcion = Captura.Capopcion();
   }
+     public static void realizarSuma(int filas, int columnas) {
+
+        double[][] matrizA = Captura.readMat(filas, columnas);
+        double[][] matrizB = Captura.readMat(filas, columnas);
+
+        double[][] resultado = OpMatriz.suma(matrizA, matrizB);
+        System.out.println("Resultado de la suma de las matrices:");
+        imprimirMatriz(resultado);
+    }
+
+    public static void realizarProducto(int filasA, int columnasA, int filasB, int columnasB) {
+        
+        if (columnasA != filasB) {
+            System.out.println("No se puede multiplicar. El número de columnas de A debe ser igual al número de filas de B.");
+            return;
+        }
+
+        double[][] matrizA = Captura.readMat(filasA, columnasA);
+        double[][] matrizB = Captura.readMat(filasB, columnasB);
+
+        double[][] resultado = OpMatriz.producto(matrizA, matrizB);
+        System.out.println("Resultado de la multiplicación de las matrices:");
+        imprimirMatriz(resultado);
+    }
+
+    public static void realizarEscalar( int filas, int columnas) {
+        double escalar = Captura.nextDouble("Ingrese el escalar: ");
+        double[][] matriz = Captura.readMat(filas, columnas);
+        double[][] resultado = OpMatriz.escalar(escalar, matriz);
+        System.out.println("Resultado de la multiplicación de la matriz por el escalar:");
+        imprimirMatriz(resultado);
+    }
+
+    public static void realizarTransposicion(int filas, int columnas) {
+        
+        System.out.println("Ingrese las dimensiones de la matriz a transponer.");
+        double[][] matriz = Captura.readMat(filas, columnas);
+        double[][] resultado = OpMatriz.tras(matriz);
+        System.out.println("Resultado de la transposición de la matriz:");
+        imprimirMatriz(resultado);
+    }
+
+    public static void imprimirMatriz(double[][] matriz) {
+        int filas = matriz.length;
+        int columnas = matriz[0].length;
+
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+}
 }
