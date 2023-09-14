5596288b44b01195b27baaca57be820993ef07c4
JACOB1705 <141367280+JACOB1705@users.noreply.github.com>
2023-09-13 22:26:05 -0500

    Add files via upload

---
+++ labs/02/03/InterfazCar.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.interfazcar;
+
+/**
+ *
+ * @author Jacobo
+ */
+public class InterfazCar {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
