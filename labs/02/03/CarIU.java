5da34ab2acb7280402d6bda53da84924c2f531c7
JACOB1705 <141367280+JACOB1705@users.noreply.github.com>
2023-09-17 18:40:30 -0500

    Add files via upload

--- labs/02/03/CarIU.java
+++ labs/02/03/CarIU.java
@@ -30,10 +30,17 @@ public class CarIU extends javax.swing.JFrame {
         jEditorPane1 = new javax.swing.JEditorPane();
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         txt = new javax.swing.JTextField();
-        jButton1 = new javax.swing.JButton();
+        Boton_bubble = new javax.swing.JButton();
+        may_men1 = new javax.swing.JButton();
+        may_men2 = new javax.swing.JButton();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        display_ord = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
+        Boton_merge = new javax.swing.JButton();
 
         jScrollPane1.setViewportView(jEditorPane1);
 
@@ -43,11 +50,6 @@ public class CarIU extends javax.swing.JFrame {
 
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagen/mantenimiento.png"))); // NOI18N
         jLabel1.setText("jLabel1");
-
-        jLabel2.setBackground(new java.awt.Color(229, 232, 232));
-        jLabel2.setFont(new java.awt.Font("Trebuchet MS", 3, 36)); // NOI18N
-        jLabel2.setForeground(new java.awt.Color(229, 232, 232));
-        jLabel2.setText("VENTA DE CARROS USADOS");
 
         jLabel3.setBackground(new java.awt.Color(229, 232, 232));
         jLabel3.setFont(new java.awt.Font("Trebuchet MS", 3, 12)); // NOI18N
@@ -61,10 +63,56 @@ public class CarIU extends javax.swing.JFrame {
             }
         });
 
-        jButton1.setText("Ordenamiento ");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        Boton_bubble.setText("Bubble");
+        Boton_bubble.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                Boton_bubbleActionPerformed(evt);
+            }
+        });
+
+        may_men1.setText("Mayor / Menor");
+        may_men1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                may_men1ActionPerformed(evt);
+            }
+        });
+
+        may_men2.setText("Mayor / Menor");
+        may_men2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                may_men2ActionPerformed(evt);
+            }
+        });
+
+        jLabel4.setBackground(new java.awt.Color(229, 232, 232));
+        jLabel4.setFont(new java.awt.Font("Trebuchet MS", 3, 12)); // NOI18N
+        jLabel4.setForeground(new java.awt.Color(229, 232, 232));
+        jLabel4.setText("Kilometraje ");
+
+        jLabel5.setBackground(new java.awt.Color(229, 232, 232));
+        jLabel5.setFont(new java.awt.Font("Trebuchet MS", 3, 12)); // NOI18N
+        jLabel5.setForeground(new java.awt.Color(229, 232, 232));
+        jLabel5.setText("Modelo");
+
+        display_ord.setBackground(new java.awt.Color(229, 232, 232));
+        display_ord.setFont(new java.awt.Font("Trebuchet MS", 3, 12)); // NOI18N
+        display_ord.setForeground(new java.awt.Color(229, 232, 232));
+        display_ord.setText("Tipo de ordenamiento: ");
+
+        jLabel2.setBackground(new java.awt.Color(229, 232, 232));
+        jLabel2.setFont(new java.awt.Font("Trebuchet MS", 3, 24)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(229, 232, 232));
+        jLabel2.setText("COMPRAVENTA UMNG");
+
+        jLabel8.setBackground(new java.awt.Color(229, 232, 232));
+        jLabel8.setFont(new java.awt.Font("Trebuchet MS", 3, 24)); // NOI18N
+        jLabel8.setForeground(new java.awt.Color(229, 232, 232));
+        jLabel8.setText("VENTA DE CARROS USADOS");
+
+        Boton_merge.setText("Merge");
+        Boton_merge.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                Boton_mergeActionPerformed(evt);
             }
         });
 
@@ -73,45 +121,84 @@ public class CarIU extends javax.swing.JFrame {
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 118, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 124, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(40, 40, 40)
+                        .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGap(14, 14, 14))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(18, 18, 18)
-                        .addComponent(jLabel2))
+                        .addComponent(jLabel8, javax.swing.GroupLayout.PREFERRED_SIZE, 327, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(16, 16, 16)
+                .addComponent(Boton_bubble, javax.swing.GroupLayout.PREFERRED_SIZE, 73, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(Boton_merge, javax.swing.GroupLayout.PREFERRED_SIZE, 73, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(may_men1)
+                .addGap(18, 18, 18)
+                .addComponent(may_men2)
+                .addGap(43, 43, 43))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(16, 16, 16)
                         .addComponent(jLabel3)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(txt, javax.swing.GroupLayout.PREFERRED_SIZE, 240, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(display_ord)
+                        .addGap(82, 82, 82)))
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(119, 119, 119)
-                        .addComponent(jButton1)))
-                .addContainerGap(36, Short.MAX_VALUE))
+                        .addComponent(txt, javax.swing.GroupLayout.PREFERRED_SIZE, 240, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(0, 17, Short.MAX_VALUE)
+                        .addComponent(jLabel4)
+                        .addGap(70, 70, 70)
+                        .addComponent(jLabel5)
+                        .addGap(73, 73, 73))))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel1)
-                    .addComponent(jLabel2))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 107, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(16, 16, 16)
+                        .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(jLabel8, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 53, Short.MAX_VALUE)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel3)
                     .addComponent(txt, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 116, Short.MAX_VALUE)
-                .addComponent(jButton1)
-                .addGap(115, 115, 115))
+                .addGap(19, 19, 19)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel4)
+                    .addComponent(jLabel5)
+                    .addComponent(display_ord))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(Boton_bubble)
+                    .addComponent(may_men1)
+                    .addComponent(may_men2)
+                    .addComponent(Boton_merge))
+                .addGap(162, 162, 162))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
@@ -121,9 +208,23 @@ public class CarIU extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_txtActionPerformed
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void Boton_bubbleActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Boton_bubbleActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jButton1ActionPerformed
+        display_ord.setText(display_ord.getText()+"Bubblesort");
+    }//GEN-LAST:event_Boton_bubbleActionPerformed
+
+    private void may_men1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_may_men1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_may_men1ActionPerformed
+
+    private void may_men2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_may_men2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_may_men2ActionPerformed
+
+    private void Boton_mergeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Boton_mergeActionPerformed
+        // TODO add your handling code here:
+        display_ord.setText(display_ord.getText()+"Mergesort");
+    }//GEN-LAST:event_Boton_mergeActionPerformed
 
     /**
      * @param args the command line arguments
@@ -161,13 +262,20 @@ public class CarIU extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
+    private javax.swing.JButton Boton_bubble;
+    private javax.swing.JButton Boton_merge;
+    private javax.swing.JLabel display_ord;
     private javax.swing.JEditorPane jEditorPane1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel8;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JButton may_men1;
+    private javax.swing.JButton may_men2;
     private javax.swing.JTextField txt;
     // End of variables declaration//GEN-END:variables
 }
