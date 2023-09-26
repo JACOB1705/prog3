13871ef825227ef6c68f239013e46793de21dacd
JACOB1705 <141367280+JACOB1705@users.noreply.github.com>
2023-09-25 19:41:26 -0500

    Add files via upload

--- labs/03/Lab0301/src/main/java/Asignatura.java
+++ labs/03/Lab0301/src/main/java/Asignatura.java
@@ -1,7 +1,39 @@
+    
+/**
+ *
+ * @author Jacobo
+ */
+
 public class Asignatura {
     int codigo;
     String nombre;
     int semestre;
     int num_creditos;
-    String horario; 
+    String horario;
+    
+     public Asignatura(int codigo, String nombre, int semestre, int num_creditos,
+            String horario) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.semestre = semestre;
+        this.num_creditos = num_creditos;
+        this.horario = horario;
+    }
+
+    public int getSemestre() {
+        return semestre;
+    }
+    
+    public int getNum_creditos() {
+        return num_creditos;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%-7d  %-35s%5d%7s", codigo, nombre, num_creditos, horario);
+    }
 }
