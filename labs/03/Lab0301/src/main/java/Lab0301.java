7e65f1581921db8223c7edea0e7129505bcb169d
JACOB1705 <141367280+JACOB1705@users.noreply.github.com>
2023-09-25 19:48:50 -0500

    Add files via upload

--- labs/03/Lab0301/src/main/java/Lab0301.java
+++ labs/03/Lab0301/src/main/java/Lab0301.java
@@ -1,34 +1,79 @@
-import java.util.LinkedList;
-import java.util.Queue;
+
+import java.util.List;
+import java.util.Scanner;
 
 
-public class Lab0301{
-    static Queue<Registro> regis = new LinkedList<>();
-    public static void  main (String[] args) {
+/**
+ *
+ * @author Jacobo
+ */
+public class Lab0301 {
+    public static void main(String[] args) 
+    {
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.print("Ingrese codigo de estudiante: ");
+        String Codigo = scanner.nextLine();
+
+        System.out.print("Ingrese su nombre: ");
+        String Nombre = scanner.nextLine();
+
+        System.out.print("Ingrese su correo institucional: ");
+        String Correo = scanner.nextLine();
+
+        System.out.print("Ingrese el semestre actal: ");
+        int Semestre = scanner.nextInt();
+
+        Estudiante estudiante = new Estudiante(Codigo, Nombre, Correo, Semestre);
+
+       
+        List<Asignatura> asignaturasDisponibles = CargarAsignaturasDesdeArchivo("Asignaturas.txt");
+
         
-        registro_estudiante();
-        registro_asignatura();
-        registro_registro_final();
+        while (true) 
+        {
+            System.out.println("\nMenú de Opciones:");
+            System.out.println("1. Registrar asignatura");
+            System.out.println("2. Mostrar asignaturas");
+            System.out.println("3. Generar lista en archivo");
+            System.out.println("4. Salir");
+            System.out.print("Selecciona una opcion: ");
+
+            int Opcion = scanner.nextInt();
+
+            switch (Opcion) 
+            {
+                case 1 -> {
+                    System.out.println("Asignaturas disponibles:");
+                    for (int i = 0; i < asignaturasDisponibles.size(); i++) 
+                    {
+                        System.out.println(i + 1 + ". " + asignaturasDisponibles.get(i));
+                    }
+                    System.out.print("Seleccione el numero de asignatura que va a cursar: ");
+                    int NumeroAsignatura = scanner.nextInt();
+                    
+                    if (NumeroAsignatura >= 1 && NumeroAsignatura <= asignaturasDisponibles.size()) 
+                    {
+                        Asignatura asignaturaSeleccionada = asignaturasDisponibles.get(NumeroAsignatura - 1);
+                        estudiante.registrarAsignatura(asignaturaSeleccionada);
+                    } else 
+                    {
+                        System.out.println("Numero de asignatura no válido.");
+                    }
+                }
+                    case 2 -> estudiante.MostrarReporte();
+                    case 3 -> estudiante.GenerarReporteArchivo("reporte_asignaturas.txt");
+                    case 4 -> {
+                    System.out.println("operacion finalizada");
+                    scanner.close();
+                    System.exit(0);
+                }
+                default -> System.out.println("Opcion no valida.");
+            }
+        }
     }
+
+
     
-    
-    public static void registro_estudiante(){
-        
-        String codigo = Entrada.readText("Nombre Estudiante");
-        String nombre = Entrada.readText("Nombre Estudiante: ");
-        String correo = Entrada.readText("Correo Estudiante: ");   
-        int semestre = Entrada.readInt("Semestre");
-        Estudiante estud = new Estudiante(codigo, nombre, correo, semestre);
-        regis.add(new Registro(estud));
-    }
-    
-    public static void registro_asignatura(){
-        
-        
-    }
-    
-    public static void registro_registro_final(){
-        
-        
-    }
 }
