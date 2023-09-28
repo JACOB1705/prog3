11f21db83a7c1428cdd7d120223c37dda12a2c92
JACOB1705 <141367280+JACOB1705@users.noreply.github.com>
2023-09-28 14:58:00 -0500

    Add files via upload

---
+++ labs/04/Ej0403/src/main/java/Ej0403.java
@@ -0,0 +1,93 @@
+import java.util.TimerTask;
+import java.util.Timer;
+
+public class Ej0403 extends javax.swing.JFrame {
+
+    Timer timer;
+    TimerTask timertask;
+    Boolean funcionando = false;
+ 
+    public Ej0403() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setText("jLabel3");
+
+        jButton1.setText("jButton1");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap(165, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addGap(177, 177, 177))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(jButton1)
+                        .addGap(160, 160, 160))))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(38, 38, 38)
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 122, Short.MAX_VALUE)
+                .addComponent(jButton1)
+                .addGap(101, 101, 101))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        if (!funcionando) {
+            Timer timer = new Timer();
+            timertask = new TimerTask() {
+                int i = 5;
+                @Override
+                public void run() {
+                    jLabel1.setLocation(i, 19); //Ubica el jLabel1 en las
+                    i++; //coordenadas (i,19)
+                }
+            };
+            timer.scheduleAtFixedRate(timertask, 0, 100);//cada 100ms se
+            // ejecuta el código del TimerTask
+            funcionando = true;
+            jButton1.setText("Detener");
+        } else {
+            timer.cancel(); //Cancela el Timer
+            funcionando = false;
+            jButton1.setText("Iniciar");
+        }
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Ej0403().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
+    // End of variables declaration//GEN-END:variables
+}
