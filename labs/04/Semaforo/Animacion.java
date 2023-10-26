9af82e1619bf90aa8bfab81d395ae6791c90227e
JACOB1705 <141367280+JACOB1705@users.noreply.github.com>
2023-10-26 14:09:17 -0500

    Create Animacion.java

---
+++ labs/04/Semaforo/Animacion.java
@@ -0,0 +1,9 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+public class Animacion {
+    public static void main(String [] args){
+       
+    }
+}
