70def61bfe6be332d779b25d960c80c7202b5eb0
JACOB1705 <141367280+JACOB1705@users.noreply.github.com>
2023-10-26 14:38:53 -0500

    Update Aux.java

--- labs/04/Semaforo/Aux.java
+++ labs/04/Semaforo/Aux.java
@@ -1,9 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+import static java.lang.Thread.sleep;
+import java.awt.Color;
+import javax.swing.*;
 
-public class Aux {
-  public static void main(String[] args) {
-      
+public class Aux implements Runnable {
+    public boolean rep;
+    public JButton rojo;
+    public JButton amarillo;
+    public JButton verde;
+    public JTextField texto;
+    public int num=0, temp1=0, temp2=0, temp3=0, temp4=0;
+   
+    public Aux(JButton entrada1, JButton entrada2, JButton entrada3, JTextField entrada4){
+        this.rojo=entrada1;
+        this.amarillo=entrada2;
+        this.verde=entrada3;
+        this.texto=entrada4;
     }
-}
