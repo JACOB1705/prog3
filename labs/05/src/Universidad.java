42b655a3f143a86ee7892d7c32a20495ae0b742b
JACOB1705 <141367280+JACOB1705@users.noreply.github.com>
2023-11-01 18:56:21 -0500

    Create Universidad.java

---
+++ labs/05/src/Universidad.java
@@ -0,0 +1,11 @@
+public class Universidad {
+    int matriculados;
+    String universidades;
+    String Departamento; 
+    
+    public Universidad(int matriculados, String universidades, String Departamento) {
+        this.matriculados = matriculados;
+        this.universidades = universidades;
+        this.Departamento = Departamento;
+    }
+}
