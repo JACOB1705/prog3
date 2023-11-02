8cb56ad0297de155282972847df658e1936a2fce
JACOB1705 <141367280+JACOB1705@users.noreply.github.com>
2023-11-01 19:31:47 -0500

    Update LecData.java

--- labs/05/src/LecData.java
+++ labs/05/src/LecData.java
@@ -1,3 +1,8 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class LecData {
@@ -6,4 +11,22 @@ public class LecData {
     public  LecData() {
         universidades = new ArrayList<>();
         cargarUniversidadesDesdeArchivo();
+    }
+ private void cargarUniversidadesDesdeArchivo() {
+        try (Scanner scanner = new Scanner(new File("datos.txt"))) {
+            while (scanner.hasNextLine()) {
+                String[] datosUniversidad = scanner.nextLine().split(",");
+                if (datosTramo.length >= 10) { 
+                    Universidad universidad = new Universidad(Integer.parseInt(datosUniversidad[1]), datosUniversidad[2], datosUniversidad[3]);
+                    universidades.add(universidad);
+                    System.out.println(datosUniversidad[1]+"a");
+                } else {
+                    System.out.println("La línea no cuenta con el espacio suficiente: " + Arrays.toString(datosUniversidad));
+                }
+            }
+            String currentDirectory = System.getProperty("user.dir");
+            System.out.println("a"+scanner.hasNextLine()+currentDirectory);
+        } catch (FileNotFoundException e) {
+            System.out.println("Error al leer el archivo de departamentos: " + e.getMessage());
+        }
     }
