6165e3b50c473481a5424e591f6504d5147803a7
JACOB1705 <141367280+JACOB1705@users.noreply.github.com>
2023-11-08 22:16:40 -0500

    Add files via upload

--- labs/06/Casino/src/TragaMonedas.java
+++ labs/06/Casino/src/TragaMonedas.java
@@ -41,10 +41,11 @@ public class TragaMonedas extends javax.swing.JFrame {
         Ranura4 = new javax.swing.JLabel();
         girar = new javax.swing.JButton();
         premio = new javax.swing.JTextField();
+        titulo = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jPanel1.setBackground(new java.awt.Color(0, 102, 204));
+        jPanel1.setBackground(new java.awt.Color(102, 51, 255));
         jPanel1.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
 
         Ranura1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Iconos/icono1.jpg"))); // NOI18N
@@ -55,7 +56,7 @@ public class TragaMonedas extends javax.swing.JFrame {
 
         Ranura4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Iconos/icono4.jpg"))); // NOI18N
 
-        girar.setBackground(new java.awt.Color(255, 102, 102));
+        girar.setBackground(new java.awt.Color(51, 153, 255));
         girar.setFont(new java.awt.Font("Berlin Sans FB", 0, 18)); // NOI18N
         girar.setForeground(new java.awt.Color(255, 255, 255));
         girar.setText("GIRAR");
@@ -65,50 +66,62 @@ public class TragaMonedas extends javax.swing.JFrame {
             }
         });
 
+        premio.setEditable(false);
+        premio.setBackground(new java.awt.Color(102, 51, 255));
+        premio.setBorder(null);
         premio.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 premioActionPerformed(evt);
             }
         });
 
+        titulo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Iconos/titulo.png"))); // NOI18N
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap(63, Short.MAX_VALUE)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                        .addComponent(Ranura1, javax.swing.GroupLayout.PREFERRED_SIZE, 174, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(Ranura2, javax.swing.GroupLayout.PREFERRED_SIZE, 174, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(18, 18, 18)
-                        .addComponent(Ranura3, javax.swing.GroupLayout.PREFERRED_SIZE, 174, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(Ranura4, javax.swing.GroupLayout.PREFERRED_SIZE, 174, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(47, 47, 47))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                        .addComponent(girar, javax.swing.GroupLayout.PREFERRED_SIZE, 246, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(300, 300, 300))))
-            .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(388, 388, 388)
                 .addComponent(premio, javax.swing.GroupLayout.PREFERRED_SIZE, 71, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(0, 0, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(55, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(girar, javax.swing.GroupLayout.PREFERRED_SIZE, 246, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(300, 300, 300))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(titulo)
+                        .addGap(328, 328, 328))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(Ranura1)
+                        .addGap(24, 24, 24)
+                        .addComponent(Ranura2)
+                        .addGap(26, 26, 26)
+                        .addComponent(Ranura3)
+                        .addGap(38, 38, 38)
+                        .addComponent(Ranura4, javax.swing.GroupLayout.PREFERRED_SIZE, 152, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(50, 50, 50))))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(134, 134, 134)
+                .addGap(43, 43, 43)
+                .addComponent(titulo)
+                .addGap(48, 48, 48)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(Ranura1, javax.swing.GroupLayout.PREFERRED_SIZE, 168, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(Ranura2, javax.swing.GroupLayout.PREFERRED_SIZE, 168, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(Ranura3, javax.swing.GroupLayout.PREFERRED_SIZE, 168, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(Ranura4, javax.swing.GroupLayout.PREFERRED_SIZE, 168, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
+                    .addComponent(Ranura1, javax.swing.GroupLayout.PREFERRED_SIZE, 135, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(Ranura3)
+                    .addComponent(Ranura4)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(Ranura2)
+                        .addGap(9, 9, 9)))
+                .addGap(54, 54, 54)
                 .addComponent(girar, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(27, 27, 27)
                 .addComponent(premio, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(33, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -199,5 +212,6 @@ public class TragaMonedas extends javax.swing.JFrame {
     private javax.swing.JButton girar;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JTextField premio;
+    private javax.swing.JLabel titulo;
     // End of variables declaration//GEN-END:variables
 }
